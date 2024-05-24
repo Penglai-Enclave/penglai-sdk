df8feb9881c9f4f04a132cd27b2cbdb5c999045d
wyl <2548340423@qq.com>
2024-05-24 10:10:52 +0800

    update script
    Signed-off-by: wyl <2548340423@qq.com>

--- build_sign_tool.sh
+++ build_sign_tool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -v $(pwd):/workspace -w /workspace -it registry.cn-hangzhou.aliyuncs.com/dteegen/sign_tool:1.0.0 /bin/bash -c '
+docker run -v $(pwd):/workspace -w /workspace registry.cn-hangzhou.aliyuncs.com/dteegen/sign_tool:1.0.0 /bin/bash -c '
 cd lib/host && make clean && GCC=gcc ARC=ar LINK=ld make && cd ../..
 cd lib/gm && make clean && GCC=gcc ARC=ar LINK=ld make && cd ../..
 SDK=$(pwd) && cd sign_tool && make clean && PENGLAI_SDK=$SDK make
